ffbc546c45d940a6665cc4e7224eebb9c2789312
Henning Gebhard <henning.gebhard@rub.de>
2018-12-03 13:27:31 +0100

    test case with 'Rechtsextremismus' category

--- wikispiders/spiders/history_spider.py
+++ wikispiders/spiders/history_spider.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
+from urllib.parse import parse_qs, urlsplit
+import re
+
 import scrapy
+
+# Ignore revisions older than 3 years.
+MAX_DAYS_AGE = 1095
 
 
 class HistorySpider(scrapy.Spider):
     name = "history"
-    # start_urls = [
-    #     'https://de.wikipedia.org/wiki/Kategorie:Rechtsextremismus',
-    # ]
+    now = datetime.now()
 
-    def start_requests(self, categories=['Rechtsextremismus']):
+    def start_requests(self):
+        template = 'https://de.wikipedia.org/wiki/Kategorie:{}'
+        # Default value, if there were no command line arguments.
+        categories = getattr(self, 'cats', 'Rechtsextremismus')
+        categories = categories.split(',')
+
         for cat in categories:
-            url = 'https://de.wikipedia.org/wiki/Kategorie:%s' % cat
-            yield scrapy.Request(url=url, callback=self.parse_category)
+            url = template.format(cat)
+            request = scrapy.Request(url=url, callback=self.parse_category)
+            request.meta['category'] = cat
+            yield request
 
     def parse_category(self, response):
         # history_base = [
@@ -27,11 +39,58 @@ class HistorySpider(scrapy.Spider):
                 pagename,
                 '&action=history'
             ])
-            print(history_url)
-            yield scrapy.Request(url=history_url, callback=self.parse_history)
-
+            # print(history_url)
+            request = scrapy.Request(url=history_url, callback=self.parse_history)
+            request.meta['category'] = response.meta['category']
+            request.meta['pagename'] = pagename
+            yield request
    
-
     def parse_history(self, response):
-        print(response.url)
-        pass
+        # Determine the datetime offset of this history page.
+        offset = self._parse_offset(response.url)
+        if offset and (self.now - offset).days > MAX_DAYS_AGE:
+            return
+
+        for item in response.css('ul#pagehistory li'):
+            user = item.css('span.history-user bdi::text').extract_first()
+            date = item.css('a.mw-changeslist-date::text').extract_first()
+            minor = item.css('abbr.minoredit').extract_first() != None
+            history_size = item.css('span.history-size::text').extract_first()
+            change_size = item.css('span.mw-plusminus-pos::text').extract_first()
+            if change_size is None:
+                change_size = item.css('span.mw-plusminus-neg::text').extract_first()
+            # In theory, there is a mediawiki marker for changes which revert
+            # to previous versions, but these are often times not present.
+            # So we need to use heuristics to find out if a given version is 
+            # a revert.
+            revert = False
+            comment = item.css('span.comment::text').extract_first()
+            if comment is not None:
+                revert = 'rückgängig' in comment
+            yield {
+                'user': user,
+                'date': date,
+                'minor': minor,
+                'history_size': history_size,
+                'change_size': change_size,
+                'revert': revert,
+                'category': response.meta['category'],
+                'pagename': response.meta['pagename'],
+            }
+        # After parsing all the revision items, look if there is another page 
+        # in the page history.
+        next_page = response.css('div#mw-content-text a.mw-nextlink').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse_history)          
+
+    def _parse_offset(self, url):
+        """Offset strings are of the form `yyyymmddhhmmss`."""
+        queries = parse_qs(urlsplit(url).query)
+        try:
+            offset = self._parse_offset(queries['offset'])
+            pattern = re.compile(r'(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})')   
+            (year, month, day, hours, minutes, seconds) = pattern.match(offset).groups()
+            dt = datetime(int(year), int(month), int(day), int(hours), int(minutes), int(seconds))
+        except:
+            dt = None    
+        return dt
